3fa1d10d4e7a7d32eb89648f62dfd1fd3376b999
mreckah <156549514+mreckah@users.noreply.github.com>
2025-01-16 00:11:06 +0100

    Update redisService.js

--- src/services/redisService.js
+++ src/services/redisService.js
@@ -1,13 +1,32 @@
 // Question : Comment gérer efficacement le cache avec Redis ?
-// Réponse :
+// Réponse :Utiliser des stratégies d'expiration adaptées, invalider le cache lors de mises à jour et surveiller les performances.
 // Question: Quelles sont les bonnes pratiques pour les clés Redis ?
-// Réponse :
+// Réponse :Utiliser un format structuré, éviter les clés trop longues et définir des durées d'expiration.
 
+const redisClient = require('../config/db'); 
 // Fonctions utilitaires pour Redis
 async function cacheData(key, data, ttl) {
     // TODO: Implémenter une fonction générique de cache
+    try {
+      await redisClient.set(key, JSON.stringify(data), 'EX', ttl);
+      console.log(`✅ Cached data under key: ${key}`);
+    } catch (error) {
+      console.error('❌ Error caching data:', error);
+    }
   }
+  // Fonction pour récupérer des données mises en cache
+async function getCachedData(key) {
+  try {
+    const data = await redisClient.get(key);
+    return data ? JSON.parse(data) : null; // Retourne null si aucune donnée n'est trouvée
+  } catch (error) {
+    console.error('❌ Error retrieving cached data:', error);
+    return null;
+  }
+}
   
   module.exports = {
     // TODO: Exporter les fonctions utilitaires
+    cacheData,
+  getCachedData,
   };
